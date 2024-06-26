bc3197d620a99ac07b44b1b43012879fd92fb831
Priyanshu Sharma <101499662+priyanshusharma44@users.noreply.github.com>
2024-06-26 21:00:10 +0545

    Add files via upload

---
+++ NetBeansProjects/Background/src/main/java/com/mycompany/background/Background.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.background;
+
+/**
+ *
+ * @author serjo
+ */
+public class Background {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
